b75c991c3fe5d0f6f0b2655a87a4880b10759dba
Anthony Franklin <frank@macbook-pro.attlocal.net>
2022-05-03 01:20:18 -0500

    add js for navbar on each page

---
+++ nav.js
@@ -0,0 +1,16 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+    <div class="nav">
+        <><h1><a id="logo" href="./index.html">mugen</a></h1><div class="nav-items">
+            <a href="#">decor</a>
+            <a href="#">apparel</a>
+            <a href="#">infinity.fm</a>
+            <a href="#"><img src="img/cart.png" alt="shopping-cart"></></a>
+        </div></>
+    </div>
+    `;
+}
+
+createNav();
